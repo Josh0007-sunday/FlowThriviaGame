09164de15380664cab8c289e36212cecdecfeaae
Joshua sunday <joshuasunday300@gmail.com>
2025-05-18 18:52:12 +0100

    added audio

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AdminPage } from "./pages/adminPage";
 import { GamePage } from "./pages/gamePage";
 import "./config/flow-config";
+import gameAudio from "./assets/gmaudio.mp3";
+
 
 function App() {
   return (
     <Router>
+      <audio src={gameAudio} autoPlay loop />
       <Routes>
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/" element={<GamePage />} />
